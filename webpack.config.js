0d7c1b2321475c8c625e6dc172b3a8d23f9f32e4
Marc <marc.mcintyre@emso.com>
2017-07-09 23:09:27 +0100

    Use prod version. Updated to latest version.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+                NODE_ENV: JSON.stringify('production')
             }
         })
     ],
