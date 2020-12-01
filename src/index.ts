3895940cef436e5b1d07fb8e0d543f3e067e22da
Marc McIntyre <Marc.McIntyre@emso.com>
2020-12-01 11:59:37 +0000

    We need to export everything.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as StickyTree } from './StickyTree';
+export * from './StickyTree';
 export { default as AutoSizedStickyTree } from './AutoSizedStickyTree';
+export *  from './AutoSizedStickyTree';
